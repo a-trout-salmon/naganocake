caffc42445161c36136151bfbe6c8abb22e4a50a
miya38-hub <miyacharge3803@gmail.com>
2026-03-19 05:56:48 +0000

    管理者注文詳細追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,25 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.includes(:customer, :order_details)
                    .order(created_at: :desc)
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.includes(:item)
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order), notice: "更新しました"
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
